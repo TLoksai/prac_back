36aaaa5f15e0d6df5c25891e26d894a2b6726992
Loksai <loksai2024@gmail.com>
2025-04-04 15:57:02 +0530

    update : signup and signin

--- backend/db.py
+++ backend/db.py
@@ -3,5 +3,5 @@ from motor.motor_asyncio import AsyncIOMotorClient
 MONGO_URI = "mongodb://localhost:27017"
 client = AsyncIOMotorClient(MONGO_URI)
 
-db = client["user_auth_db"]  # Your DB name
-users_collection = db["users"]  # Your collection name
+db = client["user_auth_db"]  #  DB name
+users_collection = db["users"]  # collection name
--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,8 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from routes import login, signup
-
-
-
+from routes import signup, login
 
 app = FastAPI()
 
-# CORS middleware to allow frontend requests
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Adjust for frontend port
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# Register routes separately
-app.include_router(signup.router, prefix="/auth", tags=["Signup"])
-app.include_router(login.router, prefix="/auth", tags=["Login"])
-
-
+# Routes
+app.include_router(signup.router, prefix="/auth")
+app.include_router(login.router, prefix="/auth")
