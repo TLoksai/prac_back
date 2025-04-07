387e6730142c087ad098d49b73a7a913795b92f5
Loksai <loksai2024@gmail.com>
2025-04-07 11:42:51 +0530

    updated

--- backend/db.py
+++ backend/db.py
@@ -1,6 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URI = "mongodb://localhost:27017"
+MONGO_URI = "mongodb+srv://loksai2024:<db_password>@cluster0.5hn4myx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = AsyncIOMotorClient(MONGO_URI)
 
 db = client["user_auth_db"]  #  DB name
--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:8000/auth"; // Ensure this matches your FastAPI backend
+const API_BASE_URL = "https://prac-back-14.onrender.com/auth"; // Ensure this matches your FastAPI backend
 
 const AuthForm = () => {
   const [isSignup, setIsSignup] = useState(false);
